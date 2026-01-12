c4f3f4ba96a585020df3c18f5f10cad5c52880c2
wilfried-wayou <wilfried.wayou@2026.ucac-icam.com>
2026-01-12 17:15:38 +0100

    Remove hardcoded region from MinIO client configuration in MinioConfig.

--- job/src/main/java/com/etsia/job/infrastructure/controller/dto/CreateJobOfferRequest.java
+++ job/src/main/java/com/etsia/job/infrastructure/controller/dto/CreateJobOfferRequest.java
@@ -1,5 +1,6 @@
 package com.etsia.job.infrastructure.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.etsia.common.infrastructure.security.UserIdAware;
 import com.etsia.common.infrastructure.entities.JobType;
 import lombok.Data;
@@ -16,6 +17,7 @@ public class CreateJobOfferRequest implements UserIdAware {
     private JobType type;
     private String location;
     private String salary;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS", timezone = "UTC")
     private Instant deadline;
     private String applicationUrl;
     private Integer publisherId;
